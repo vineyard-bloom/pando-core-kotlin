90c9ab8f52bb006ab6fdd61a9b2304b024915ad5
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-05-29 14:11:53 -0600

    Writing validateBlockchain function

--- projects/pandocore/src/main/kotlin/pando/Validation.kt
+++ projects/pandocore/src/main/kotlin/pando/Validation.kt
@@ -3,5 +3,16 @@ package pando
 typealias ValidationErrors = List<Error>
 
 fun validateBlockchain(blockchain: Blockchain): ValidationErrors {
-  return listOf(Error("Not implemented"))
+  return blockchain.blocks.flatMap { block ->
+    if (hashBlock(BlockHashContents(
+                    address = block.address,
+                    valueType = ValueType.long,
+                    transactionHashes = block.transactions.map { it.hash },
+                    previousBlock = getBlockHash(block.previousBlock),
+                    createdAt = block.createdAt
+            )) == block.hash)
+      listOf()
+    else
+      listOf(Error("Incorrect hash has been found in block " + block.index))
+  }
 }
