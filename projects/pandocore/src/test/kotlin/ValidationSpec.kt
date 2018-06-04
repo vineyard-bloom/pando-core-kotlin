08d7ad0cf163afe24eb72435ba90b1f052a43e48
dwleonhardt <dwleonhardt@gmail.com>
2018-06-04 15:21:05 -0600

    validateBlock address test

--- projects/pandocore/src/test/kotlin/ValidationSpec.kt
+++ projects/pandocore/src/test/kotlin/ValidationSpec.kt
@@ -22,8 +22,22 @@ class ValidationSpec : Spek({
       val transaction = createTransaction(100L, "nowhere", mining.address)
       val b = createBlock(genesis, transaction, privateKey)
       val badBlock = Block("Bad Hash", BlockContents(b.contents.index, b.contents.address, b.contents.transaction, b.contents.previousBlock, b.contents.createdAt), b.blockSignatures)
-      val (blockchain, errors) = validateBlock(badBlock, genesis.publicKey, genesis)
+      val (_, errors) = validateBlock(badBlock, genesis.publicKey, genesis)
       assertEquals(3, errors.size)
+    }
+
+    it("can detect bad block address") {
+      val (genesis, privateKey) = utility.createNewBlockchain()
+      val (secondBlockchain, privateKeyTwo) = utility.createNewBlockchain()
+      val mining = mintTokens(genesis, 1000)
+      val miningTwo = mintTokens(secondBlockchain, 1000)
+      val transaction = createTransaction(100L, "nowhere", mining.address)
+      val transactionTwo = createTransaction(100L, "nowhere", miningTwo.address)
+      val b = createBlock(genesis, transaction, privateKey)
+      val bTwo = createBlock(secondBlockchain, transactionTwo, privateKeyTwo)
+      val badBlock = Block(b.hash, BlockContents(b.contents.index, bTwo.address, b.contents.transaction, b.contents.previousBlock, b.contents.createdAt), b.blockSignatures)
+      val (_, errors) = validateBlock(badBlock, genesis.publicKey, genesis)
+      assertEquals(2, errors.size)
     }
 
   }
