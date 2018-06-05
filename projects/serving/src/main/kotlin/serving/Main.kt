e5d652a58407efc9d0d736cccb2a4dd555548197
dwleonhardt <dwleonhardt@gmail.com>
2018-06-05 17:07:17 -0600

    get blockchain by address logic working

--- projects/serving/src/main/kotlin/serving/Main.kt
+++ projects/serving/src/main/kotlin/serving/Main.kt
@@ -7,29 +7,27 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import pando.BlockchainSource
+import pando.*
+
+fun createServer(source: BlockchainSource): NettyApplicationEngine{
+  return embeddedServer(Netty, port = 8080){
+    routing {
+      get("/address/{address}"){
+        println(source(call.parameters["address"]!!))
+        if (source(call.parameters["address"]!!)!!.address == call.parameters["address"]) {
+          call.respondText("Blockchain: ${source(call.parameters["address"]!!)}")
+        }
+        else {
+          call.respondText("No blockchain found at that address")
+        }
+      }
+    }
+  }.start(wait = true)
+
+
+}
 
 fun main(args: Array<String>) {
-  embeddedServer(Netty, port = 8080, module = Application::mainModule).start(wait = true)
+  createServer({null})
 }
 
-fun Application.mainModule() {
-
-  routing {
-    root()
-    address()
-  }
-
-}
-
-fun Routing.root() {
-  get("/") {
-    call.respondText("Nothing to see here")
-  }
-}
-
-fun Routing.address() {
-  get("/address/{address}"){
-    call.respondText("Address: ${call.parameters["address"]}")
-  }
-}
--- projects/serving/src/test/kotlin/ServerSpec.kt
+++ projects/serving/src/test/kotlin/ServerSpec.kt
@@ -2,19 +2,20 @@ import junit.framework.TestCase.*
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import java.net.URL
 import serving.*
 import pando.*
+import java.net.HttpURLConnection
 
 
-class BlockchainSpec : Spek({
+class ServerSpec : Spek({
   describe("server requests") {
 
     it("can get blockchain from address") {
       val pair = generateAddressPair()
       val blockchains = createNewBlockchain(pair.address, pair.keyPair.public)
-
       val source = { address:Address -> blockchains }
-
+      createServer(source)
       assert(true)
     }
 
