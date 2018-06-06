7579d030bcb76a4f9f673234727d406b5c1e1289
dwleonhardt <dwleonhardt@gmail.com>
2018-06-06 10:46:04 -0600

    get request from ServerSpec r&d

--- projects/serving/src/test/kotlin/ServerSpec.kt
+++ projects/serving/src/test/kotlin/ServerSpec.kt
@@ -1,22 +1,30 @@
-import junit.framework.TestCase.*
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import java.net.URL
 import serving.*
 import pando.*
-import java.net.HttpURLConnection
+import io.ktor.client.*
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.config
+import io.ktor.client.request.*
+import junit.framework.TestCase.*
 
 
 class ServerSpec : Spek({
   describe("server requests") {
 
     it("can get blockchain from address") {
+      val client = HttpClient(CIO.config {
+
+      })
       val pair = generateAddressPair()
-      val blockchains = createNewBlockchain(pair.address, pair.keyPair.public)
-      val source = { address:Address -> blockchains }
+      val blockchain = createNewBlockchain(pair.address, pair.keyPair.public)
+      val source = { address:Address -> blockchain }
+
       createServer(source)
-      assert(true)
+      val res = client.get<String>("http://0.0.0.0:8080/address/${blockchain.address}")
+
+      assertEquals(blockchain.address, res)
     }
 
   }
