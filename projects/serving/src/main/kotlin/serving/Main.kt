6f9c6f5a315ef3923f7d6a8810ca70eea13ef9cf
dwleonhardt <dwleonhardt@gmail.com>
2018-06-07 13:27:41 -0600

    add JacksonConverter() to server for JSON conversion

--- projects/serving/src/main/kotlin/serving/Main.kt
+++ projects/serving/src/main/kotlin/serving/Main.kt
@@ -1,33 +1,42 @@
 package serving
 
 import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.ApplicationEngine
 import io.ktor.server.engine.embeddedServer
-import pando.Address
-import pando.BlockchainSource
-import pando.createNewBlockchain
-import pando.generateAddressPair
+import io.ktor.jackson.*
+import io.ktor.features.*
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import pando.*
 
 fun createServer(source: BlockchainSource): ApplicationEngine {
+
   return embeddedServer(CIO, port = 8080) {
+    install(ContentNegotiation) {
+      jackson {
+        register(ContentType.Application.Json, JacksonConverter())
+      }
+    }
     routing {
       get("/") {
         call.respondText("hey")
       }
-      get("/address/{address}") {
+      get("/blockchain/{address}") {
         println(source(call.parameters["address"]!!)!!.address)
         if (source(call.parameters["address"]!!)!!.address == call.parameters["address"]) {
-          call.respondText("${source(call.parameters["address"]!!)}")
+          call.respondText("${source(call.parameters["address"]!!)}", ContentType.Application.Json)
         } else {
-          call.respondText("No Blockchain at address: ${call.parameters["address"]}")
+          call.respondText("No blockchain found at address: ${call.parameters["address"]}")
         }
       }
     }
-  }.start(wait = false)
+  }.start(wait = true)
 }
 
 fun main(args: Array<String>) {
@@ -37,4 +46,3 @@ fun main(args: Array<String>) {
 
   createServer(source)
 }
-
