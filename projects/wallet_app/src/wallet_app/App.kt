ce75ad0e243972b2ef0dd72d414d6fdea5fbd06e
dwleonhardt <dwleonhardt@gmail.com>
2018-06-12 09:35:17 -0600

    add blockchain address table to the wallet application

--- projects/wallet_app/src/wallet_app/App.kt
+++ projects/wallet_app/src/wallet_app/App.kt
@@ -5,14 +5,13 @@ import javafx.animation.Timeline
 import javafx.application.Application
 import javafx.event.EventHandler
 import javafx.scene.Scene
-import javafx.scene.control.Button
-import javafx.scene.control.TextArea
+import javafx.scene.control.*
+import javafx.scene.control.cell.PropertyValueFactory
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyCodeCombination
 import javafx.scene.input.KeyCombination
 import javafx.scene.input.KeyEvent
 import javafx.scene.layout.VBox
-import javafx.scene.text.Font
 import javafx.stage.Stage
 import javafx.util.Duration
 import jsoning.parseJsonFile
@@ -20,28 +19,89 @@ import jsoning.saveJson
 import networking.primitiveBlockchain
 import pando.*
 import java.io.File
+import javafx.beans.property.SimpleStringProperty
+import javafx.collections.FXCollections
+
+
+
 
 data class Keys(
     val publicKey: String,
     val privateKey: String
 )
 
-val keyDirectory = "addresses"
+
+class Address constructor(address: String, publicKey: String, privateKey: String) {
+  private val address: SimpleStringProperty
+  private val publicKey: SimpleStringProperty
+  private val privateKey: SimpleStringProperty
+
+  init {
+    this.address = SimpleStringProperty(address)
+    this.publicKey = SimpleStringProperty(publicKey)
+    this.privateKey = SimpleStringProperty(privateKey)
+  }
+
+  fun getAddress(): String {
+    return address.get()
+  }
+
+  fun setAddress(fName: String) {
+    address.set(fName)
+  }
+
+  fun getPublicKey(): String {
+    return publicKey.get()
+  }
+
+  fun setPublicKey(fName: String) {
+    publicKey.set(fName)
+  }
+
+  fun getPrivateKey(): String {
+    return privateKey.get()
+  }
+
+  fun setPrivateKey(fName: String) {
+    privateKey.set(fName)
+  }
+
+}
+val keyDirectory = "out/bin/addresses"
+
+
 
 class AppWindow : Application() {
 
   override fun start(primaryStage: Stage) {
     primaryStage.title = "Pando Wallet"
-    val textArea = TextArea()
-    textArea.setPrefRowCount(10)
-    textArea.setFont(Font.font("Courier", 14.0))
+    val addressCol = TableColumn<Address, String>("Address")
+    val publicCol = TableColumn<Address, String>("Public Key")
+    val privateCol = TableColumn<Address, String>("Private Key")
+    val tableView = TableView<Address>()
+    addressCol.setCellValueFactory(
+      PropertyValueFactory<Address, String>("address")
+    )
+    publicCol.setCellValueFactory(
+      PropertyValueFactory<Address, String>("publicKey")
+    )
+    privateCol.setCellValueFactory(
+      PropertyValueFactory<Address, String>("privateKey")
+    )
+
+    tableView.getSelectionModel().setCellSelectionEnabled(true)
+
+    val data = FXCollections.observableArrayList<Address>()
+
+    tableView.getColumns().addAll(addressCol, publicCol, privateCol)
+
+    tableView.setItems(data);
 
     val newBlockchain = Button()
     newBlockchain.text = "Create Blockchain"
     newBlockchain.onAction = EventHandler {
       val pair = generateAddressPair()
       val blockchain = createNewBlockchain(pair.address, pair.keyPair.public)
-      textArea.text = blockchain.toString()
       val primitiveBlockchain = primitiveBlockchain(blockchain)
 
       val newKeys = Keys(
@@ -55,19 +115,13 @@ class AppWindow : Application() {
       saveJson(newKeys, keyDirectory + "/" + blockchain.address)
     }
 
-    val getAddresses = Button()
-    getAddresses.text = "Get Addresses"
-    getAddresses.onAction = EventHandler {
-      File(keyDirectory).walkTopDown().forEach {
-        if (it.extension == "json") {
-          val keys = parseJsonFile<Keys>(it)
-          val publicKey = stringToPublicKey(keys.publicKey)
-          val privateKey = stringToPrivateKey(keys.privateKey)
-          val pubTest = keyToString(publicKey)
-          println(pubTest)
-          val privTest = keyToString(privateKey)
-          println(privTest)
-        }
+    File(keyDirectory).walk().forEach {
+      if (it.extension == "json") {
+        val address = File(it.toString()).nameWithoutExtension
+        val keys = parseJsonFile<Keys>(it)
+        val publicKey = stringToPublicKey(keys.publicKey)
+        val privateKey = stringToPrivateKey(keys.privateKey)
+        data.add(Address(address, keys.publicKey, keys.privateKey))
       }
     }
 
@@ -77,10 +131,10 @@ class AppWindow : Application() {
     updater.play()
 
     val root = VBox()
-    root.children.add(textArea)
+    root.children.add(tableView)
     root.children.add(newBlockchain)
-    root.children.add(getAddresses)
-    primaryStage.scene = Scene(root, 600.0, 300.0)
+
+    primaryStage.scene = Scene(root, 1400.0, 500.0)
 
     primaryStage.scene.addEventFilter(KeyEvent.KEY_PRESSED, object : EventHandler<KeyEvent> {
       internal val keyComb: KeyCombination = KeyCodeCombination(KeyCode.S,
